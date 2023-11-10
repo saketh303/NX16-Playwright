5f7b6467ef0a7b5927a61681b39ce9c51b801138
Vu Nguyen <vu.nguyen3950@gmail.com>
2023-11-10 11:28:50 -0500

    pushed timeout to step

--- apps/playwright-e2e/step-definitions/test.step.ts
+++ apps/playwright-e2e/step-definitions/test.step.ts
@@ -2,7 +2,7 @@ import { createEnvironment } from '@ngx-playwright/harness';
 import { Given, Then } from '@cucumber/cucumber';
 import { firefox } from '@playwright/test'
 
-Given('I am on the homepage', async () => {
+Given('I am on the homepage', {timeout: 10000}, async () => {
     const browser = await firefox.launch();
     const context = await browser.newContext();
     const page = await context.newPage();
