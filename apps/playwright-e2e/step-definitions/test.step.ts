337156357bb0d303ae4f4a32643de2fe7b7f335f
Caleb Ukle <caleb@nrwl.io>
2023-11-13 16:17:30 -0600

    fix: use esm dep within cucumber step file
    use node register api to load ts-node/register hooks within esm cucumber esm file

--- apps/playwright-e2e/step-definitions/test.step.ts
+++ apps/playwright-e2e/step-definitions/test.step.ts
@@ -1,13 +1,13 @@
-import { createEnvironment } from '@ngx-playwright/harness';
-import { Given, Then } from '@cucumber/cucumber';
-import { firefox } from '@playwright/test'
+import { createEnvironment } from "@ngx-playwright/harness";
+import { Given, Then } from "@cucumber/cucumber";
+import { firefox } from "@playwright/test";
 
-Given('I am on the homepage', {timeout: 10000}, async () => {
-    const browser = await firefox.launch();
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    await page.goto('http://localhost:4200');
-    const harness = createEnvironment(page);
-    await browser.close();
-    return 0;
+Given("I am on the homepage", { timeout: 10000 }, async () => {
+  const browser = await firefox.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("http://localhost:4200");
+  const harness = createEnvironment(page);
+  await browser.close();
+  return 0;
 });
